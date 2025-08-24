dad96dd84a171fec772db91c4235c077bd32fbca
Joseph Lee <jiosephlee@gmail.com>
2025-08-24 02:16:44 -0400

    change .sh

--- scripts/FT/fine_tune.sh
+++ scripts/FT/fine_tune.sh
@@ -1,2 +1,2 @@
-num_epochs=200
-nohup python finetuning_knowledge_v6.py --experiment_name SingleArxivPaper_1B_Full_Finetuning_Test_Run_${num_epochs}_Epochs --num_train_epochs $num_epochs --learning_rate 2e-5 --full_finetuning > output.log 2>&1 &
+num_epochs=10
+nohup python finetuning_knowledge_v6.py --experiment_name ParaphrasedArxivPaper_1B_Full_Finetuning_Test_Run_${num_epochs}_Epochs --num_train_epochs $num_epochs --learning_rate 2e-5 --full_finetuning > output.log 2>&1 &
