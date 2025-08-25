09172410beab90223e0eff0835960393c1ff71c1
Joseph Lee <jiosephlee@gmail.com>
2025-08-25 02:20:57 -0400

    ran paraphrased tests +updated .sh

--- scripts/FT/fine_tune.sh
+++ scripts/FT/fine_tune.sh
@@ -1,3 +1,7 @@
 num_epochs=100
-paraphrased=_Paraphrased_25
-nohup python finetuning_knowledge_v6.py --experiment_name ParaphrasedArxivPaper_1B_Full_Finetuning_Test_Run_${num_epochs}_Epochs${} --num_train_epochs $num_epochs --learning_rate 2e-5 --full_finetuning > output.log 2>&1 &
+num_paraphrased=25
+yes_paraphrased=_Paraphrased_${num_paraphrased}
+echo $num_paraphrased
+echo $num_epochs
+python finetuning_knowledge_v6.py --experiment_name ParaphrasedArxivPaper_1B_Full_Finetuning_Test_Run_${num_epochs}_Epochs${yes_paraphrased} --num_train_epochs $num_epochs --learning_rate 2e-5 --num_paraphrased_texts $num_paraphrased --full_finetuning> output.log 2>&1 &
+python finetuning_knowledge_v6.py --experiment_name SingleArxivPaper_1B_Full_Finetuning_Test_Run_${num_epochs}_Epochs --num_train_epochs $num_epochs --learning_rate 2e-5 --num_paraphrased_texts $num_paraphrased --full_finetuning> output_2.log 2>&1 &
