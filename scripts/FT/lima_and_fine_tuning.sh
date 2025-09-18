8b4868250c89edde846f62addf0132ed67754962
Joseph Lee <jiosephlee@gmail.com>
2025-09-18 13:59:04 -0400

    ran paraphrase but it was run with broken paraphrase text (title missing); running again

--- scripts/FT/lima_and_fine_tuning.sh
+++ scripts/FT/lima_and_fine_tuning.sh
@@ -1,9 +1,10 @@
 num_epochs=100
-num_paraphrased=0
+num_paraphrased=9
 nohup python finetuning_knowledge_v8.py \
     --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
     --effective_batch_size_for_cpt 32 \
     --separate_batches_with_pretraining 1 \
+    --pretraining_data_type arxiv \
     --fill_batches_with_pretraining \
     --num_train_epochs $num_epochs \
     --learning_rate 2e-5 \
