ce761bcd3107a4f0a9a6085c69d1c3578fc4fadc
Joseph Lee <jiosephlee@gmail.com>
2025-09-18 13:44:05 -0400

    fixed paraphrase experiments; added experiemnt scripst

---
+++ scripts/FT/E1_7B_single.sh
@@ -0,0 +1,17 @@
+num_epochs=100
+num_paraphrased=0
+nohup python finetuning_knowledge_v8.py \
+    --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
+    --effective_batch_size_for_cpt 32 \
+    --separate_batches_with_pretraining 1 \
+    --fill_batches_with_pretraining \
+    --num_train_epochs $num_epochs \
+    --learning_rate 2e-5 \
+    --num_paraphrased_texts $num_paraphrased \
+    --overlap_sections \
+    --overlap_ratio 1_4 \
+    --lima_afterwards \
+    --full_finetuning > output_1.log 2>&1 &
+
+        # --separate_batches_with_pretraining 1 \
+
--- scripts/FT/E4_1B_data_replay_0_times.sh
+++ scripts/FT/E4_1B_data_replay_0_times.sh
@@ -1,18 +1,16 @@
 num_epochs=100
 num_paraphrased=0
-echo "Experiment 1: Using single arxiv paper but with overlapping sections."\
-
-nohup python finetuning_knowledge_v7.py \
+nohup python finetuning_knowledge_v8.py \
+    --device_batch_size 8 \
+    --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
+    --effective_batch_size_for_cpt 32 \
+    --fill_batches_with_pretraining \
     --num_train_epochs $num_epochs \
     --learning_rate 2e-5 \
     --num_paraphrased_texts $num_paraphrased \
     --overlap_sections \
     --overlap_ratio 1_4 \
-    --do_eval \
     --lima_afterwards \
     --full_finetuning > output_1.log 2>&1 &
 
-    #--custom_suffix "no_overlapping_abalation_no_title" \
-
-
-    
+# This may takes around 4 hours to run on 3090 GPU
