6f4bb3551f811757d1ee8ecfe88050cf90877513
Joseph Lee <jiosephlee@gmail.com>
2025-09-19 00:51:40 -0400

    fixing bash script for prior knowledge

--- scripts/FT/prior_knowledge.sh
+++ scripts/FT/prior_knowledge.sh
@@ -8,6 +8,8 @@ nohup python finetuning_prior_knowledge_v1.py \
     --num_train_epochs $num_epochs \
     --learning_rate 4e-5 \
     --lima_afterwards \
+    --push_to_hub_cpt_id prior_7B \
+    --push_to_hub_lima_id prior_7B_with_lima \
     --full_finetuning > output_1.log 2>&1 &
 
         # --separate_batches_with_pretraining 1 \
