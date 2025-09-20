44388a59740a9cfca453371885822385c5ae3a49
Joseph Lee <jiosephlee@gmail.com>
2025-09-19 22:41:28 -0400

    changing test script

--- scripts/FT/finetuning_knowledge_v8.py
+++ scripts/FT/finetuning_knowledge_v8.py
@@ -552,7 +552,7 @@ if __name__ == "__main__":
 
     if args.test_script:
         log.info("--- RUNNING IN TEST SCRIPT MODE ---")
-        args.num_train_epochs = 2
+        args.num_train_epochs = 0
         args.base_results_dir = os.path.join("../../results", "tests")
     else: 
         os.environ["WANDB_PROJECT"]="fine_tuning_study"
