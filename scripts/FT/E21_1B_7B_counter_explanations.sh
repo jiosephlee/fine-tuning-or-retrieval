bc3c8d202f83f8243e9e2ba668663a43c20b162a
Joseph Lee <jiosephlee@gmail.com>
2025-09-19 22:15:51 -0400

    converted asserts to log warnings

--- scripts/FT/E21_1B_7B_counter_explanations.sh
+++ scripts/FT/E21_1B_7B_counter_explanations.sh
@@ -1,10 +1,11 @@
 num_epochs=100
 num_paraphrased=9
-nohup python finetuning_knowledge_v8.py \
-    --model_id allenai/OLMo-2-1124-7B \
+python finetuning_knowledge_v8.py \
     --device_batch_size 4 \
+    --with_explanations \
     --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
     --effective_batch_size_for_cpt 32 \
+    --separate_batches_with_pretraining 1 \
     --fill_batches_with_pretraining \
     --num_train_epochs $num_epochs \
     --learning_rate 2e-5 \
@@ -14,4 +15,6 @@ nohup python finetuning_knowledge_v8.py \
     --lima_afterwards \
     --context_length_for_lima 2560 \
     --full_finetuning > output_1.log 
-# This may take around 3 hours
+    
+# Time: 10 hours
+
