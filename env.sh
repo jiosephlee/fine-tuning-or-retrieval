a730447b7b859328f13f5f5775939e089da2859e
Joseph Lee <jiosephlee@gmail.com>
2025-09-21 19:38:36 -0400

    editing env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,9 @@
 touch utils/keys.py
+mkdir data/olmo/
 pip install flash-attn --no-build-isolation
 pip install git+https://github.com/huggingface/trl
 pip install pydantic datasets==3.6.0 peft bitsandbytes wandb matplotlib seaborn liger-kernel scikit-learn openai
-cd ..
+cd ../
 git clone https://github.com/jiosephlee/transformers
 cd tranformers/
 pip install .[torch]
