7c538e12b7b7d23357109381312b8f658b7e612f
Joseph Lee <jiosephlee@gmail.com>
2025-09-24 23:41:54 -0400

    reverting batch size for E39-2

--- scripts/FT/E39_7B_single_cleaning_ablation.sh
+++ scripts/FT/E39_7B_single_cleaning_ablation.sh
@@ -19,7 +19,7 @@ python finetuning_knowledge_v8.py \
     --model_id allenai/OLMo-2-1124-7B \
     --device_batch_size 4 \
     --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
-    --effective_batch_size_for_cpt 48 \
+    --effective_batch_size_for_cpt 32 \
     --separate_batches_with_pretraining 1 \
     --fill_batches_with_pretraining \
     --num_train_epochs $num_epochs \
