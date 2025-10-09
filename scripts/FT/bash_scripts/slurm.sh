540fc73f311ab8d31226ce6bf46a6b5810d9e723
Joseph Lee <jiosephlee@gmail.com>
2025-10-09 00:22:34 -0400

    adjusted slumrt script

--- scripts/FT/bash_scripts/slurm.sh
+++ scripts/FT/bash_scripts/slurm.sh
@@ -3,11 +3,11 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
-#SBATCH --time=07:00:00
+#SBATCH --time=01:00:00
 #SBATCH --gres=gpu:a100:1
-#SBATCH --partition=ai
+#SBATCH --partition=ai-short
 #SBATCH --mem-per-gpu=160GB
-#SBATCH --job-name=fine_tuning_study_v5
+#SBATCH --job-name=fine_tuning_study_v8
 #SBATCH --output=logs/output.out
 #SBATCH --error=logs/error.err
 
@@ -19,8 +19,22 @@ echo "➤ SET UP CUDA"
 module unload cuda
 module load cuda/12.4
 
-experiment_name="SingleArxivPaper_1B_Test_Run"
-
 echo "➤ RUN SCRIPT"
-python finetuning_knowledge_v5.py --experiment_name "$experiment_name" --num_train_epochs 100 --learning_rate 2e-5 --full_finetuning
+num_epochs=1
+num_paraphrased=0
+python finetuning_knowledge_v8.py \
+    --model_id Qwen/Qwen2.5-7B\
+    --device_batch_size 1 \
+    --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
+    --effective_batch_size_for_cpt 32 \
+    --separate_batches_with_pretraining 1 \
+    --fill_batches_with_pretraining \
+    --num_train_epochs $num_epochs \
+    --learning_rate 2e-5 \
+    --num_paraphrased_texts $num_paraphrased \
+    --overlap_sections \
+    --overlap_ratio 1_4 \
+    --lima_afterwards \
+    --context_length_for_lima 2560 \
+    --full_finetuning > output_1.log 
 echo "➤ DONE"
