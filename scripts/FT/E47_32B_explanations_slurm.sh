e9234dca3fd1971b42d47cf5a667a55ad0ebdda0
Joseph Lee <jiosephlee@gmail.com>
2025-11-10 06:41:54 -0500

    fixed

--- scripts/FT/E47_32B_explanations_slurm.sh
+++ scripts/FT/E47_32B_explanations_slurm.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --time=1:00:00
-#SBATCH --gpus-per-node:a100:3
+#SBATCH --gpus-per-node=a100:3
 #SBATCH --partition=ai
 #SBATCH --mem-per-gpu=160GB
 #SBATCH --job-name=E47_32B_explanations_if_it_fails
