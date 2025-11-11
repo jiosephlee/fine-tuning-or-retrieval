f958a26eefa0f2f495aabd03e67d338ab3772df1
Yidi Huang <yidih@upenn.edu>
2025-11-11 10:23:15 -0500

    finish E48, 49

--- scripts/FT/E45_7B_10_epochs.sh
+++ scripts/FT/E45_7B_10_epochs.sh
@@ -2,7 +2,7 @@ num_epochs=10
 num_paraphrased=0
 python finetuning_knowledge_v8.py \
     --model_id allenai/OLMo-2-1124-7B \
-    --device_batch_size 4 \
+    --device_batch_size 2 \
     --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
     --effective_batch_size_for_cpt 32 \
     --separate_batches_with_pretraining 1 \
@@ -12,13 +12,13 @@ python finetuning_knowledge_v8.py \
     --num_paraphrased_texts $num_paraphrased \
     --overlap_sections \
     --overlap_ratio 1_4 \
-    --full_finetuning > output_44_a.log   
+    --full_finetuning > output_45_a.log   
 
 num_epochs=10
 num_paraphrased=9
 python finetuning_knowledge_v8.py \
     --model_id allenai/OLMo-2-1124-7B \
-    --device_batch_size 4 \
+    --device_batch_size 2 \
     --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
     --effective_batch_size_for_cpt 32 \
     --separate_batches_with_pretraining 1 \
@@ -28,6 +28,6 @@ python finetuning_knowledge_v8.py \
     --num_paraphrased_texts $num_paraphrased \
     --overlap_sections \
     --overlap_ratio 1_4 \
-    --full_finetuning > output_44_b.log   
+    --full_finetuning > output_45_b.log   
 
 # 2 hours
