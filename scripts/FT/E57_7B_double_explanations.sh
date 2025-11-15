ff55dd36edf2f8a476a803bc8ef98022c47d2d76
Joseph Lee <jiosephlee@gmail.com>
2025-11-15 16:49:10 -0500

    two new scripts

--- scripts/FT/E57_7B_double_explanations.sh
+++ scripts/FT/E57_7B_double_explanations.sh
@@ -2,7 +2,7 @@ num_epochs=100
 num_paraphrased=9
 python finetuning_knowledge_v8.py \
     --model_id allenai/OLMo-2-1124-7B \
-    --device_batch_size 2 \
+    --device_batch_size 1 \
     --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
     --effective_batch_size_for_cpt 32 \
     --separate_batches_with_pretraining 1 \
@@ -16,5 +16,25 @@ python finetuning_knowledge_v8.py \
     --constant_lr \
     --with_explanations \
     --explanation_every_round \
-    --full_finetuning > output_57.log   
+    --full_finetuning > output_57_a.log   
+
+num_epochs=100
+num_paraphrased=9
+python finetuning_knowledge_v8.py \
+    --model_id allenai/OLMo-2-1124-7B \
+    --device_batch_size 1 \
+    --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
+    --effective_batch_size_for_cpt 32 \
+    --separate_batches_with_pretraining 1 \
+    --fill_batches_with_pretraining \
+    --num_train_epochs $num_epochs \
+    --learning_rate 2e-5 \
+    --num_paraphrased_texts $num_paraphrased \
+    --overlap_sections \
+    --overlap_ratio 1_4 \
+    --gradient_checkpointing \
+    --constant_lr \
+    --with_explanations \
+    --explanation_every_round \
+    --full_finetuning > output_57_b.log   
 
