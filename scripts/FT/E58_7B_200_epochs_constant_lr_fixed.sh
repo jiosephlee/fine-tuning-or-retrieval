55f99f6d5c1b569b30ffcedd0551ad4d5b394953
Joseph Lee <jiosephlee@gmail.com>
2025-11-18 13:06:48 -0500

    new parc script

--- scripts/FT/E58_7B_200_epochs_constant_lr_fixed.sh
+++ scripts/FT/E58_7B_200_epochs_constant_lr_fixed.sh
@@ -1,15 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=run
+#SBATCH --output=slurm-%j.out
+#SBATCH --time=12:00:00
+#SBATCH --partition=dgx-b200          # example GPU partition on Betty
+#SBATCH --gpus=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128GB
+
 num_epochs=200
 num_paraphrased=9
 
-# Define the SIF and the script
-SIF_FILE="/cbica/home/leejose/finetune_pytorch_2_4.sif" # Or the full path
-# Execute the job with the runtime fix
-apptainer exec \
-    --nv \
-    ${SIF_FILE} \
-    python finetuning_knowledge_v8.py \
+python finetuning_knowledge_v8.py \
     --model_id allenai/OLMo-2-1124-7B \
-    --device_batch_size 1 \
+    --device_batch_size 32 \
     --override_domains DPO 1_58 GRPO BOFT OFT QLoRA \
     --effective_batch_size_for_cpt 32 \
     --separate_batches_with_pretraining 1 \
@@ -20,8 +23,8 @@ apptainer exec \
     --num_paraphrased_texts $num_paraphrased \
     --overlap_sections \
     --overlap_ratio 1_4 \
+    --attn_implementation flash_attention_3 \
     --gradient_checkpointing \
-    --offload_to_cpu \
-    --full_finetuning > output_46_a.log   
+    --full_finetuning
 
 # 20 hours
