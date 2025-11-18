9afb860201576e791d21c82235826ec785ef15a8
Joseph Lee <jiosephlee@gmail.com>
2025-11-18 13:12:04 -0500

    re run flash attn

--- env_slurm.sh
+++ env_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=flash_attention_install
 #SBATCH --output=slurm-%j.out
-#SBATCH --time=01:00:00
+#SBATCH --time=00:30:00
 #SBATCH --partition=dgx-b200          # example GPU partition on Betty
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=16
@@ -10,8 +10,5 @@
 module load anaconda3
 source "$(conda info --base)/etc/profile.d/conda.sh"
 conda activate "$HOME/envs/finetuning"
-pip install flash-attn --no-build-isolation
-pip install git+https://github.com/huggingface/trl
-pip install pydantic datasets==3.6.0 peft bitsandbytes wandb matplotlib seaborn liger-kernel scikit-learn openai
-
+"$HOME/envs/finetuning/bin/pip" install flash-attn --no-build-isolation
 nvidia-smi || true
